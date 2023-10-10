66f12e761dfd50e9e451ee78940d49ab16bc8611
ibrahim kaba Datascientest <ikaba49@gmail.com>
2023-10-10 08:54:44 +0000

    last dance

--- app/main.py
+++ app/main.py
@@ -3,4 +3,4 @@ from fastapi import FastAPI
 app = FastAPI()
 @app.get("/")
 def read_root():
-    return {"Hello": "We Love Datascientest, and we did it. We build a CI/CD pipeline <3 8"}
+    return {"Hello": "We Love Datascientest, and we did it. We build a CI/CD pipeline <3 8 last chance"}
